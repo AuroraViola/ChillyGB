5eb1ad1bd38dafd9b838e2e75d8e080f7673c4a8
AuroraViola <110638310+AuroraViola@users.noreply.github.com>
2024-06-04 01:10:22 +0200

    Implemente palettes, sprite flip, sprite priority and window

--- ppu.c
+++ ppu.c
@@ -20,6 +20,36 @@ void decode_tile(cpu *c, uint16_t addr, uint8_t tile[8][8]) {
     }
 }
 
+void flipY_tile(uint8_t tile[8][8]) {
+    uint8_t tilecopy[8][8];
+    for (uint8_t i = 0; i < 8; i++) {
+        for (uint8_t j = 0; j < 8; j++) {
+            tilecopy[i][j] = tile[i][j];
+        }
+    }
+
+    for (uint8_t i = 0; i < 8; i++) {
+        for (uint8_t j = 0; j < 8; j++) {
+            tile[i][j] = tilecopy[7-i][j];
+        }
+    }
+}
+
+void flipX_tile(uint8_t tile[8][8]) {
+    uint8_t tilecopy[8][8];
+    for (uint8_t i = 0; i < 8; i++) {
+        for (uint8_t j = 0; j < 8; j++) {
+            tilecopy[i][j] = tile[i][j];
+        }
+    }
+
+    for (uint8_t i = 0; i < 8; i++) {
+        for (uint8_t j = 0; j < 8; j++) {
+            tile[i][j] = tilecopy[i][7-j];
+        }
+    }
+}
+
 void load_sprites(cpu *c, ppu *p) {
     for (int i = 0; i < 40; i++) {
         for (int j = 0; j < 4; j++) {
@@ -109,16 +139,31 @@ void load_window(cpu *c, ppu *p) {
 }
 
 void load_display(cpu *c, ppu *p) {
+    uint8_t bg_pal = c->memory[0xff47];
+    uint8_t bg_palette[] = { (bg_pal & 3), ((bg_pal & 12) >> 2), ((bg_pal & 48) >> 4), (bg_pal >> 6) };
+    uint8_t obp0 = c->memory[0xff48];
+    uint8_t obp1 = c->memory[0xff49];
+    uint8_t s_palette[4];
     load_background(c, p);
     load_window(c, p);
     if ((c->memory[0xff40] & 128) != 0) {
-        uint8_t scx = c->memory[0xff43];
-        uint8_t scy = c->memory[0xff42];
-        uint8_t wy = c->memory[0xff4a];
-        uint8_t wx = c->memory[0xff4b];
-        for (uint8_t y = 0; y < 144; y++) {
-            for (uint8_t x = 0; x < 160; x++) {
-                p->display[y][x] = p->background[(uint8_t) (y + scy)][(uint8_t) (x + scx)];
+        if ((c->memory[0xff40] & 1) != 0) {
+            uint8_t scx = c->memory[0xff43];
+            uint8_t scy = c->memory[0xff42];
+            for (uint8_t y = 0; y < 144; y++) {
+                for (uint8_t x = 0; x < 160; x++) {
+                    p->display[y][x] = bg_palette[p->background[(uint8_t) (y + scy)][(uint8_t) (x + scx)]];
+                }
+            }
+        }
+        if (((c->memory[0xff40] & 32) != 0) && ((c->memory[0xff40] & 1) != 0)) {
+            uint8_t wy = c->memory[0xff4a];
+            uint8_t wx = c->memory[0xff4b];
+            for (uint8_t y = 0; y < 144; y++) {
+                for (uint8_t x = 0; x < 167; x++) {
+                    if (((y + wy) < 144) && ((x + wx - 7) < 160) && ((x + wx - 7) >= 0))
+                        p->display[y + wy][x + wx - 7] = p->window[y][x];
+                }
             }
         }
         if ((c->memory[0xff40] & 2) != 0) {
@@ -128,10 +173,32 @@ void load_display(cpu *c, ppu *p) {
                     uint8_t sprite_tile[8][8];
                     decode_tile(c, (0x8000 | (p->sprites[i][2] << 4)), sprite_tile);
 
+                    if (((p->sprites[i][3] >> 4) & 1) != 0) {
+                        s_palette[0] = obp1 & 3;
+                        s_palette[1] = (obp1 >> 2) & 3;
+                        s_palette[2] = (obp1 >> 4) & 3;
+                        s_palette[3] = (obp1 >> 6) & 3;
+                    }
+                    else {
+                        s_palette[0] = obp0 & 3;
+                        s_palette[1] = (obp0 >> 2) & 3;
+                        s_palette[2] = (obp0 >> 4) & 3;
+                        s_palette[3] = (obp0 >> 6) & 3;
+                    }
+
+                    // X flip
+                    if (((p->sprites[i][3] >> 5) & 1) != 0)
+                        flipX_tile(sprite_tile);
+
+                    // Y flip
+                    if (((p->sprites[i][3] >> 6) & 1) != 0)
+                        flipY_tile(sprite_tile);
+
                     for (int16_t y = 0; y < 8; y++) {
                         for (int16_t x = 0; x < 8; x++) {
                             if (sprite_tile[y][x] != 0) {
-                                p->display[p->sprites[i][0] + y - 16][p->sprites[i][1] + x - 8] = sprite_tile[y][x];
+                                if ((((p->sprites[i][3] >> 7) & 1) == 0) || (p->display[p->sprites[i][0] + y - 16][p->sprites[i][1] + x - 8] == 0))
+                                    p->display[p->sprites[i][0] + y - 16][p->sprites[i][1] + x - 8] = s_palette[sprite_tile[y][x]];
                             }
                         }
                     }
