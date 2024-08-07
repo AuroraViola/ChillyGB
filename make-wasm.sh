ccb06578e1ebbe0b6eeabbe6e27fe91d6c0c0e46
AuroraViola <110638310+AuroraViola@users.noreply.github.com>
2024-08-07 17:59:24 +0200

    fixed audio bug when changing window in WASM

--- make-wasm.sh
+++ make-wasm.sh
@@ -8,7 +8,7 @@ emcc -o build-wasm/index.html \
 	-I. -Iraylib/src/ -L. -Lraylib/src/ -s USE_GLFW=3 \
 	-DPLATFORM_WEB \
 	-sEXPORTED_RUNTIME_METHODS=ccall \
-	-sEXPORTED_FUNCTIONS=_load_settings,_pause,_load_cartridge,_main \
+	-sEXPORTED_FUNCTIONS=_load_settings,_pause_game,_load_cartridge,_main \
 	--shell-file=wasm-frontend/shell.html \
 	-lidbfs.js \
 	--preload-file ./res
