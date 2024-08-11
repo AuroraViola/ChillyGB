8a2470e7bed76e77464fa31663c6575f81127245
Aurora Arcidiacono <110638310+AuroraViola@users.noreply.github.com>
2024-08-11 18:30:18 +0200

    Changed checkbox style

--- raylib-nuklear/include/nuklear.h
+++ raylib-nuklear/include/nuklear.h
@@ -24778,13 +24778,14 @@ nk_draw_checkbox(struct nk_command_buffer *out,
 
     /* draw background and cursor */
     if (background->type == NK_STYLE_ITEM_COLOR) {
-        nk_fill_rect(out, *selector, 0, nk_rgb_factor(style->border_color, style->color_factor));
-        nk_fill_rect(out, nk_shrink_rect(*selector, style->border), 0, nk_rgb_factor(background->data.color, style->color_factor));
+        //nk_fill_rect(out, *selector, 0, nk_rgb_factor(style->border_color, style->color_factor));
+        //nk_fill_rect(out, nk_shrink_rect(*selector, style->border), 0, nk_rgb_factor(background->data.color, style->color_factor));
+        nk_stroke_rect(out, *selector, 2, 2, background->data.color);
     } else nk_draw_image(out, *selector, &background->data.image, nk_rgb_factor(nk_white, style->color_factor));
     if (active) {
         if (cursor->type == NK_STYLE_ITEM_IMAGE)
             nk_draw_image(out, *cursors, &cursor->data.image, nk_rgb_factor(nk_white, style->color_factor));
-        else nk_fill_rect(out, *cursors, 0, cursor->data.color);
+        else nk_fill_rect(out, *cursors, 0, background->data.color);
     }
 }
 NK_LIB void
