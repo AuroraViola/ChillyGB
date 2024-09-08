5629b8665ebab7658740eebd4b5dc454a8244683
AuroraViola <110638310+AuroraViola@users.noreply.github.com>
2024-09-08 19:03:33 +0200

    Bumped save state version

--- src/modules/savestates.c
+++ src/modules/savestates.c
@@ -5,7 +5,7 @@
 #include <string.h>
 #include <stdio.h>
 
-const int savestate_version = 6;
+const int savestate_version = 7;
 
 void get_save_state_name(char rom_name[256], char save_state_name[256]) {
     strcpy(save_state_name, rom_name);
