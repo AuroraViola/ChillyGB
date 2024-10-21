0945b41574ab5ef8844171682feee7f7fe289eb7
AuroraViola <110638310+AuroraViola@users.noreply.github.com>
2024-10-21 21:38:50 +0200

    Fixed submodules directory

--- make-wasm.sh
+++ make-wasm.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-mkdir -p build-wasm
-cp res/icons/ChillyGB.svg build-wasm/icon.svg
+mkdir -p build-wasm &&
+cp res/icons/ChillyGB.svg build-wasm/icon.svg &&
 emcc -o build-wasm/index.html \
 	src/main.c src/modules/* third-party/cJSON/cJSON.c third-party/sr_webcam/src/sr_webcam.c third-party/sr_webcam/src/sr_webcam_lin.c\
 	-Os -Wall third-party/raylib/src/libraylib.a \
@@ -11,9 +11,9 @@ emcc -o build-wasm/index.html \
 	-sEXPORTED_FUNCTIONS=_load_settings,_pause_game,_load_cartridge,_main,_in_game \
 	--shell-file=wasm-frontend/shell.html \
 	-lidbfs.js \
-	--preload-file ./res
+	--preload-file ./res &&
 
-cp -r wasm-frontend/* build-wasm/
-rm build-wasm/shell.html
+cp -r wasm-frontend/* build-wasm/ &&
+rm build-wasm/shell.html &&
 # Add build date to service worker, so it will update on every build
 echo "// Built on $(date) - HEAD:$(git rev-parse HEAD)" >> build-wasm/serviceWorker.js
